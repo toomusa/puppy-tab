a025da19c9d56eddc80927e9c57ff52a0c6c2600
Musa Akbari <musa.akbari@gmail.com>
2020-11-03 12:18:13 -0800

    ready app

--- public/app.js
+++ public/app.js
@@ -1,29 +1,31 @@
 console.log("connected");
 
-$(document).on("click", "#startBtn", async (event) => {
-  $("#startBtn").attr("disabled", true);
-  $("#stopBtn").attr("disabled", false);
-  $("#startBtn").text("Checking");
+$(document).on("click", "#startBtn", async () => {
   let count = 0;
-  const click = () => {
-    console.log(count + " clicks");
-    count++;
-  }
+  $("#startBtn").attr("disabled", true).text("Checking");
+  $("#stopBtn").attr("disabled", false);
 
-  $(document).on("click", "#stopBtn", async (event) => {
+  $(document).on("click", "#stopBtn", async () => {
     $("#stopBtn").attr("disabled", true);
-    $("#startBtn").attr("disabled", false);
+    $("#startBtn").attr("disabled", false).text("Start Checking");
     clearInterval(interval);
   });
 
   const checkPups = async () => {
     await $.get("/scrape", function (data) {
-      console.log(data);
+      console.log(count, data);
+      data.forEach(name => {
+        if (name === "Bella" || name === "Otis") {
+          clearInterval(interval);
+          console.log("PUPPY IS BORN")
+          $.get("https://cors-anywhere.herokuapp.com/https://maker.ifttt.com/trigger/new_post/with/key/bvTJmlsBTGVBzZbqL2yqA9", function () {
+            console.log("Notification is Sent")
+          });
+        }
+      })
     })
+    count++;
   }
 
-  // checkPups();
-
-  // const interval = setInterval(checkPups, 1000)
-  
+  const interval = setInterval(checkPups, 1000)
 })
